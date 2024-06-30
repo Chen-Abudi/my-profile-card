da7618ca727698aa84503ebeb10e3ed1e9ca64c1
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-30 23:57:35 +0300

    style: Add priority to profile image and hover to mail

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -15,9 +15,12 @@ const ProfileCard: React.FC = () => {
             className="relative object-cover object-center rounded-full w-fit h-60 mt-6 text-gray-700 bg-white shadow-md bg-clip-border"
             alt="My profile photo"
             src={myImg}
+            priority
           />
           <div className="flex flex-col items-center m-4">
-            <h1 className="text-xl font-bold">Grace Chen Abudi</h1>
+            <h1 className="text-xl font-bold text-blue-950">
+              Grace Chen Abudi
+            </h1>
             <p className="text-gray-600">Software Engineer</p>
           </div>
         </div>
--- src/components/profile/tabs/ContactTab.tsx
+++ src/components/profile/tabs/ContactTab.tsx
@@ -3,13 +3,16 @@ import React from "react";
 const ContactTab: React.FC = () => {
   return (
     <div>
-      <h2 className="text-xl font-bold">Contact</h2>
+      <h2 className="text-xl font-bold text-blue-700">Contact</h2>
       <p>
         If you have any questions or would like to collaborate on a project,
         please feel free to reach out to me. You can find me on various social
         media platforms, including GitHub, LinkedIn, and Medium. Additionally,
         you can email me at{" "}
-        <a href="mailto:cheezumcali23@gmail.com" className="text-blue-900">
+        <a
+          href="mailto:cheezumcali23@gmail.com"
+          className="font-medium text-blue-900 hover:text-blue-500"
+        >
           GMail
         </a>
         .
