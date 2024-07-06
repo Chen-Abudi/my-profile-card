d44e5ee4667b09dac0a996e3f2e162259c6433f4
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-07-06 19:31:10 +0300

    style: Output globals css into styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
 import { ReactNode } from "react";
 import React from "react";
 
